d8abf1d5d8e5433815eea1e347cb62890e07fc4a
kungumasakthivel <kungumasakthivel.k@gmail.com>
2025-01-11 16:05:54 +0530

    all good 11/1 0405

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import './App.css'
-// import HowItWorks from './components/HowItWorks/HowItWorks'
-// import Faq from './components/Faq/Faq'
-// import NavBar from './components/NavBar/NavBar'
+import HowItWorks from './components/HowItWorks/HowItWorks'
+import Faq from './components/Faq/Faq'
+import NavBar from './components/NavBar/NavBar'
 import LifeStyleIcons from './components/LifeStyleIcons/LifeStyleIcons'
 
 function App() {
   return (
     <>
-      {/* <NavBar />
-      <HowItWorks />
-      <Faq /> */}
+      <NavBar />
       <LifeStyleIcons />
+      <HowItWorks />
+      <Faq />
     </>
   )
 }

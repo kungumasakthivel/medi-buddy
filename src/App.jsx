f9ff81653de82f5f03e46de076d38fcd24064952
kungumasakthivel <kungumasakthivel.k@gmail.com>
2025-01-11 10:44:46 +0530

    HowItWorks folder completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
-import Faq from './components/Faq/Faq'
-import NavBar from './components/NavBar/NavBar'
+import HowItWorks from './components/HowItWorks/HowItWorks'
+// import Faq from './components/Faq/Faq'
+// import NavBar from './components/NavBar/NavBar'
 
 function App() {
   return (
     <>
-      <NavBar />
-      <Faq />
+      {/* <NavBar /> */}
+      {/* <Faq /> */}
+      <HowItWorks />
     </>
   )
 }

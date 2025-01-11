99c2f4eb1f924a0947a35824ee504ab1c49a523e
kungumasakthivel <kungumasakthivel.k@gmail.com>
2025-01-11 16:00:44 +0530

    life style icons done

---
+++ src/components/LifeStyleIcons/LifeStyleIcons.jsx
@@ -0,0 +1,45 @@
+import {Icon1, Icon2, Icon3} from './Icons/Icons.jsx';
+import './LifeStyleIcons.css';
+function LifeStyleIcons() {
+  return (
+    <div>
+      <h1 className="text-start mt-5 text-lg font-bold ml-5">
+        Lifestyle Health Check-up Packages
+      </h1>
+      <div className="flex justify-around mt-5">
+          
+            <div className="">
+              <Icon1 />
+              <p className='text-center'>Hyper-Tension</p>
+            </div>
+            <div className=" ">
+              <Icon2 />
+              <p>Obesity</p>
+            </div>
+            <div className=" ">
+              <Icon3 />
+              <p>Smoking</p>
+            </div>
+      </div>
+      <div className="flex justify-around mt-5">
+
+            <div className="">
+              <Icon1 />
+              <p className='text-center'>Hyper-Tension</p>
+            </div>
+
+            <div className=" ">
+              <Icon2/>
+              <p>Obesity</p>
+            </div>
+
+            <div className=" ">
+              <Icon3 />
+              <p>Smoking</p>
+            </div>  
+      </div>
+    </div>
+  )
+}
+
+export default LifeStyleIcons

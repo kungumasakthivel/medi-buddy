45751d89a2444b81479088fcbb686441cecbd9ca
kungumasakthivel <kungumasakthivel.k@gmail.com>
2025-01-12 09:52:26 +0530

    everythhing is good 0952 12/1

--- src/components/LabsVisited/Cards/Cards.jsx
+++ src/components/LabsVisited/Cards/Cards.jsx
@@ -11,6 +11,9 @@ function Cards() {
             <div className='bg-image-labs flex justify-center'>
                 <img className='src-img' src="https://th.bing.com/th/id/OIP.R0qoJBWpB48diguv5muIfgHaFj?w=222&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt='apollo image' />
             </div>
+            <div className='rating-labs'>
+                <p className="font-bold">4.5/5 (120 ratings)</p>
+            </div>
             <div className='labs-desc ml-5 mb-5'>
                 <p className='text-left text-3xl font-bold mt-5 max-sm:text-2xl'>Apollo</p>
                 <p className='text-left text-lg font-medium mt-5 max-sm:text-base'>
